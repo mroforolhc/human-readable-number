315030500d62993e846949adaa8c45ff39d0ddc2
mroforolhc <mroforolhc@gmail.com>
2021-03-15 08:40:25 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
-  
+    const ones = [
+        '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
+        'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 
+        'eighteen', 'nineteen',
+    ]
+
+    const tens = [
+        '', '', 'twenty', 'thirty', 'forty', 'fifty', 
+        'sixty', 'seventy', 'eighty', 'ninety',
+    ]
+
+    if (number === 0) return 'zero';
+    if (number < 20) return ones[number];
+
+    let answer = '';
+    if (number >= 100) {
+        answer += ones[Math.floor(number / 100)] + ' hundred ';
+        if ((number % 100) < 20) answer += ones[number % 100];
+        else answer += tens[Math.floor(number / 10) % 10] + ' ' + ones[number % 10];
+    } else if (number >= 20) {
+        answer += tens[Math.floor(number / 10)] + ' ' + ones[number % 10];
+    }
+    return answer.trim();
+
 }
